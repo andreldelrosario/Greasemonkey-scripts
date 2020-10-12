e9ddf79452d5bd1efd263c9a4a9dc9080861af7a
Andrel del Rosario <andreldelrosario@gmail.com>
2020-10-12 14:41:49 +0800

    Scripts updated

--- mangakakalots-left-right.js
+++ mangakakalots-left-right.js
@@ -1,35 +1,40 @@
-// ==UserScript==
-// @name     Left/Right Chapter Navigation for Mangakakalots
-// @namespace https://ww1.mangakakalots.com/
-// @version  0.0.2
-// @grant    none
-// @include https://*.mangakakalots.com/chapter/*
-// ==/UserScript==
+  // ==UserScript==
+  // @name     Left/Right Chapter Navigation for Mangakakalots
+  // @namespace https://mangakakalot.com/
+  // @version  0.0.3
+  // @grant    none
+  // @include https://*.mangakakalots.com/chapter/*
+  // @include https://*.mangakakalot.com/chapter/*
+  // @include https://mangakakalot.com/chapter/*
+  // ==/UserScript==
 
-const prevAnchor = [...document.getElementsByClassName("next")];
-const nextAnchor = [...document.getElementsByClassName("back")];
+  const prevAnchor = [...document.getElementsByClassName("next")];
+  const nextAnchor = [...document.getElementsByClassName("back")];
 
-let prev = prevAnchor.find(a => a.innerHTML.includes("PREV")).href;
-let next = null;
+  let prev = null;
+  let next = null;
 
-if(nextAnchor.length == 0){
-  next = prevAnchor.find(a => a.innerHTML.includes("NEXT")).href;
-} 
-else {
-  next = nextAnchor.find(a => a.innerHTML.includes("NEXT")).href;
-}
-
-function keyCheck(e){
-  // alert(e.keyCode);
-  
-  if (e.keyCode === 37) {
-    // alert(prev);
-    window.location.href = prev;
+  if (prevAnchor.length != 0) {
+    prev = prev || prevAnchor.find(a => a.innerHTML.includes("PREV")).href;
+    next = next || prevAnchor.find(a => a.innerHTML.includes("NEXT")).href;
   }
-  else if (e.keyCode === 39) {
-    // alert(next)
-    window.location.href = next;
-  }
-}
 
-window.addEventListener("keydown", keyCheck, true);
+  if (nextAnchor.length != 0){
+    prev = prev || nextAnchor.find(a => a.innerHTML.includes("PREV")).href;
+    next = next || nextAnchor.find(a => a.innerHTML.includes("NEXT")).href;
+  }
+
+  function keyCheck(e){
+    // alert(e.keyCode);
+    
+    if (e.keyCode === 37 && prev) {
+      // alert(prev);
+      window.location.href = prev;
+    }
+    else if (e.keyCode === 39 && next) {
+      // alert(next)
+      window.location.href = next;
+    }
+  }
+
+  window.addEventListener("keydown", keyCheck, true);
--- manganelo-left-right.js
+++ manganelo-left-right.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name     Left/Right Chapter Navigation for Mangakakalots
+// @name     Left/Right Chapter Navigation for Manganelo
 // @namespace https://manganelo.com/
-// @version  0.0.1
+// @version  0.0.2
 // @grant    none
 // @include https://*manganelo.com/chapter/*
 // ==/UserScript==
@@ -9,17 +9,25 @@
 const prevAnchor = [...document.getElementsByClassName("navi-change-chapter-btn-prev a-h")];
 const nextAnchor = [...document.getElementsByClassName("navi-change-chapter-btn-next a-h")];
 
-let prev = prevAnchor.find(a => a.innerHTML.includes("PREV")).href;
-let next = nextAnchor.find(a => a.innerHTML.includes("NEXT")).href;
+let prev = null;
+let next = null;
+
+if (prevAnchor.length > 0) {
+	prev = prevAnchor.find(a => a.innerHTML.includes("PREV")).href;
+}
+if (nextAnchor.length > 0) {
+	next = nextAnchor.find(a => a.innerHTML.includes("NEXT")).href;
+}
+
 
 function keyCheck(e){
   // alert(e.keyCode);
   
-  if (e.keyCode === 37) {
+  if (e.keyCode === 37 && prev) {
     // alert(prev);
     window.location.href = prev;
   }
-  else if (e.keyCode === 39) {
+  else if (e.keyCode === 39 && next) {
     // alert(next)
     window.location.href = next;
   }
