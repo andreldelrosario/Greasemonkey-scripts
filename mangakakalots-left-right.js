58cd04b6a86886449608478bdc1644dca0b0afd7
Andrel del Rosario <andreldelrosario@gmail.com>
2020-10-12 14:53:21 +0800

    Fixed mangakakalot script when reaching start or end of manga

--- mangakakalots-left-right.js
+++ mangakakalots-left-right.js
@@ -1,40 +1,40 @@
-  // ==UserScript==
-  // @name     Left/Right Chapter Navigation for Mangakakalots
-  // @namespace https://mangakakalot.com/
-  // @version  0.0.3
-  // @grant    none
-  // @include https://*.mangakakalots.com/chapter/*
-  // @include https://*.mangakakalot.com/chapter/*
-  // @include https://mangakakalot.com/chapter/*
-  // ==/UserScript==
+// ==UserScript==
+// @name     Left/Right Chapter Navigation for Mangakakalots
+// @namespace https://ww1.mangakakalots.com/
+// @version  0.0.4
+// @grant    none
+// @include https://*.mangakakalots.com/chapter/*
+// @include https://*.mangakakalot.com/chapter/*
+// @include https://mangakakalot.com/chapter/*
+// ==/UserScript==
 
-  const prevAnchor = [...document.getElementsByClassName("next")];
-  const nextAnchor = [...document.getElementsByClassName("back")];
+const prevAnchor = [...document.getElementsByClassName("next")];
+const nextAnchor = [...document.getElementsByClassName("back")];
 
-  let prev = null;
-  let next = null;
+let prev = null;
+let next = null;
 
-  if (prevAnchor.length != 0) {
-    prev = prev || prevAnchor.find(a => a.innerHTML.includes("PREV")).href;
-    next = next || prevAnchor.find(a => a.innerHTML.includes("NEXT")).href;
+if (prevAnchor.length > 0) {
+  prev = prev || prevAnchor.find(a => a.innerHTML.includes("PREV"));
+  next = next || prevAnchor.find(a => a.innerHTML.includes("NEXT"));
+}
+
+if (nextAnchor.length > 0){
+  prev = prev || nextAnchor.find(a => a.innerHTML.includes("PREV"));
+  next = next || nextAnchor.find(a => a.innerHTML.includes("NEXT"));
+}
+
+function keyCheck(e){
+  // alert(e.keyCode);
+
+  if (e.keyCode === 37 && prev) {
+    // alert(prev);
+    window.location = prev;
   }
-
-  if (nextAnchor.length != 0){
-    prev = prev || nextAnchor.find(a => a.innerHTML.includes("PREV")).href;
-    next = next || nextAnchor.find(a => a.innerHTML.includes("NEXT")).href;
+  else if (e.keyCode === 39 && next) {
+    // alert(next)
+    window.location = next;
   }
+}
 
-  function keyCheck(e){
-    // alert(e.keyCode);
-    
-    if (e.keyCode === 37 && prev) {
-      // alert(prev);
-      window.location.href = prev;
-    }
-    else if (e.keyCode === 39 && next) {
-      // alert(next)
-      window.location.href = next;
-    }
-  }
-
-  window.addEventListener("keydown", keyCheck, true);
+window.addEventListener("keydown", keyCheck, true);
